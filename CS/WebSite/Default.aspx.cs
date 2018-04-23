2bb323c8b52d79413c299b3493b3e023f4e555ba
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,19 @@
+using System;
+using DevExpress.Web.ASPxGridView;
+using DevExpress.Web.ASPxEditors;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void ASPxProgressBar1_DataBound(object sender, EventArgs e) {
+        ASPxProgressBar progressBar = (ASPxProgressBar)sender;
+
+        GridViewDataItemTemplateContainer templateContainer = (GridViewDataItemTemplateContainer)progressBar.NamingContainer;
+        object rowKeyValue = templateContainer.KeyValue;
+
+        if (progressBar.Value == null || rowKeyValue == null)
+            progressBar.Visible = false;
+        else {
+            progressBar.Attributes.Add("onclick", string.Format("OnProgressBarClickEventHandler('{0}', '{1}');", progressBar.Value, rowKeyValue));
+            progressBar.Style.Add("cursor", "pointer");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to process a click on a progress bar within a grid cell
+
+
+<p>This example demonstrates how to attach a client-side event handler to the ASPxProgressBar control inside the GridViewDataColumn's DataItemTemplate.</p>
+
+<br/>
+
+
